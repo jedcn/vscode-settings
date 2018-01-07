0b2900963213c5637604f1ec7de8633806f8ffc0
Jed Northridge <git@jedcn.com>
2018-01-06 21:57:12 -0500

    Create first pass at 'ci'

---
+++ scripts/continuous-integration.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+npm run update-markdown-from-config
+if ! git diff-index --quiet HEAD --; then
+  echo "Markdown changed when updating from config."
+  echo "This should not happen. They should be in sync."
+  exit 1
+fi
+
+npm run extract-config-from-markdown
+if ! git diff-index --quiet HEAD --; then
+  echo "Config changed when extracting from markdown."
+  echo "This should not happen. They should be in sync."
+  exit 1
+fi
